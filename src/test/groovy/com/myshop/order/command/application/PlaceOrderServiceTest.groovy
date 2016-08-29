a2f8afc97e09e7af92ef9da81a51f912d21f7ca9
bluepoet <bluepoet1112@gmail.com>
2016-08-30 01:19:52 +0900

    add PlaceOrderServiceTest(completed)

--- src/test/groovy/com/myshop/order/command/application/PlaceOrderServiceTest.groovy
+++ src/test/groovy/com/myshop/order/command/application/PlaceOrderServiceTest.groovy
@@ -1,16 +1,18 @@
 package com.myshop.order.command.application
 
+import com.myshop.catalog.domain.product.Product
+import com.myshop.catalog.domain.product.ProductId
 import com.myshop.catalog.domain.product.ProductRepository
 import com.myshop.common.model.Address
+import com.myshop.common.model.Money
 import com.myshop.member.domain.MemberId
-import com.myshop.order.command.domain.OrderRepository
-import com.myshop.order.command.domain.Orderer
-import com.myshop.order.command.domain.Receiver
-import com.myshop.order.command.domain.ShippingInfo
+import com.myshop.order.command.domain.*
+import org.mockito.ArgumentCaptor
 import spock.lang.Specification
 
 import static java.util.Collections.emptyList
 import static java.util.Collections.singletonList
+import static org.mockito.Mockito.*
 
 /**
  * Created by bluepoet on 2016. 8. 25..
@@ -22,8 +24,8 @@ class PlaceOrderServiceTest extends Specification {
 
     void setup() {
         svc = new PlaceOrderService()
-        mockProductRepository = Mock(ProductRepository)
-        mockOrderRepository = Mock(OrderRepository)
+        mockProductRepository = mock(ProductRepository)
+        mockOrderRepository = mock(OrderRepository)
         svc.setProductRepository(mockProductRepository)
         svc.setOrderRepository(mockOrderRepository)
     }
@@ -41,6 +43,51 @@ class PlaceOrderServiceTest extends Specification {
         new OrderRequest(orderer: null)              | IllegalArgumentException
         new OrderRequest(orderProducts: null)        | IllegalArgumentException
         new OrderRequest(orderProducts: emptyList()) | IllegalArgumentException
+    }
+
+    def "없는상품을 주문했을때 결과를 검증한다."() {
+        given:
+        def productId = new ProductId("P001")
+        def noProductId = new ProductId("NO_P001")
+        when(mockProductRepository.findById(productId)).thenReturn(new Product(productId, "Product 001", new Money(1000), "detail", emptyList()))
+        when(mockOrderRepository.nextOrderNo()).thenReturn(new OrderNo("mock.number"))
+        when(mockProductRepository.findById(noProductId)).thenReturn(null)
+        def orderRequest = createRequest()
+
+        when:
+        orderRequest.setOrderProducts(singletonList(new OrderProduct(noProductId.getId(), 2)))
+        svc.placeOrder(orderRequest)
+
+        then:
+        thrown(NoOrderProductException)
+
+        when:
+        orderRequest.setOrderProducts(Arrays.asList(
+                new OrderProduct(productId.getId(), 2),
+                new OrderProduct(noProductId.getId(), 2)
+        ))
+        svc.placeOrder(orderRequest)
+
+        then:
+        thrown(NoOrderProductException)
+    }
+
+    def "정상적인 주문요청일 경우 결과를 검증한다."() {
+        given:
+        def productId = new ProductId("P001")
+        when(mockProductRepository.findById(productId)).thenReturn(new Product(productId, "Product 001", new Money(1000), "detail", emptyList()))
+        when(mockOrderRepository.nextOrderNo()).thenReturn(new OrderNo("mock.number"))
+        def orderRequest = createRequest()
+
+        when:
+        svc.placeOrder(orderRequest)
+
+        then:
+        ArgumentCaptor<Order> orderCaptor = ArgumentCaptor.forClass(Order.class)
+        verify(mockOrderRepository).save(orderCaptor.capture())
+
+        Order createdOrder = orderCaptor.getValue()
+        createdOrder != null
     }
 
     def createRequest() {
